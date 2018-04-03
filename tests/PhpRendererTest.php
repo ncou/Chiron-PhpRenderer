a95aeaf23c568b5f40904081822257915a632f51
ncou <nicolas_courade@yahoo.fr>
2018-04-03 22:38:30 +0200

    update tests

--- tests/PhpRendererTest.php
+++ tests/PhpRendererTest.php
@@ -34,7 +34,7 @@ class PhpRendererTest extends TestCase
         $this->assertEquals("Hi", $response);
     }
 
-    public function testExceptionInTemplate()
+    public function testExceptionInTemplateWithCatch()
     {
         $renderer = new \Chiron\Views\PhpRenderer("tests/");
 
@@ -42,11 +42,22 @@ class PhpRendererTest extends TestCase
             $response = $renderer->render("testException.php");
         } catch (Throwable $t) { // PHP 7+
             // Simulates an error template
-            ob_end_clean();
             $response = $renderer->render("testTemplate.php", [
                 "hello" => "Hi"
             ]);
         }
+
+        $this->assertEquals("Hi", $response);
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testExceptionInTemplate()
+    {
+        $renderer = new \Chiron\Views\PhpRenderer("tests/");
+
+        $response = $renderer->render("testException.php");
 
         $this->assertEquals("Hi", $response);
     }
