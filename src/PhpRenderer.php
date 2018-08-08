269e8dbd320215fc9a6edabfe426cb8a7443f4a3
ncou <nicolas_courade@yahoo.fr>
2018-08-08 15:25:06 +0200

    update

--- src/PhpRenderer.php
+++ src/PhpRenderer.php
@@ -101,7 +101,7 @@ class PhpRenderer implements \ArrayAccess
             ob_start();
             call_user_func(function () {
                 extract(func_get_arg(1), EXTR_OVERWRITE); // EXTR_SKIP
-                require func_get_arg(0);
+                include func_get_arg(0);
             }, $template, $variables);
             $content = ob_get_clean();
         } catch (Throwable $e) {
@@ -134,20 +134,20 @@ class PhpRenderer implements \ArrayAccess
      *   Remove HTML tags (except those enumerated) and non-printable
      *   characters to mitigate XSS/code injection attacks.
      *
-     *   @param $val string
-     *   @param $tags string - tags allowed, separated by a commma, semi-colon or pipe character. EX : "b|i" or "<b>;<i>" or "b,<i>". use "*" to keep all the html tags
+     *   @param string $raw
+     *   @param string $tags - tags allowed, separated by a commma, semi-colon or pipe character. EX : "b|i" or "<b>;<i>" or "b,<i>". use "*" to keep all the html tags
      **/
-    private function scrub(string $val, string $tags = null): void
+    private function scrub(string $raw, string $tags = null): void
     {
         // if tags = *, we don't remove the html tags in the string
         if ($tags != '*') {
             //Split comma-, semi-colon, or pipe-separated string tags
             $tags = array_map('trim', preg_split('/[,;|]/', $tags, 0, PREG_SPLIT_NO_EMPTY));
-            $val = trim(strip_tags($val, '<' . implode('><', $tags) . '>'));
+            $raw = trim(strip_tags($raw, '<' . implode('><', $tags) . '>'));
         }
 
         // remove non displayable chars -> remove control characters (ASCII characters 0 to 31) except for tabs and newlines
-        echo trim(preg_replace('/[\x00-\x08\x0B-\x0C\x0E-\x1F]/', '', $val));
+        echo trim(preg_replace('/[\x00-\x08\x0B-\x0C\x0E-\x1F]/', '', $raw));
     }
 
     /********************************************************************************
